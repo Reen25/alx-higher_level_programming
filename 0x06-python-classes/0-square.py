efd3ae29fbe36903e437585f1ef6d8761429d621
Reen25 <mumodoreen25@gmail.com>
2023-02-15 05:15:56 -0800

    force name

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+    """ this module defines a empty class square """
+
+        
+
+            
+
+                class Square:
+
+                            """ empty class for task 0 """
+
+                                    pass
+
+
+
+
