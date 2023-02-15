82f1676ff64ee05780fb8f11ee1673dc90dda1e4
Reen25 <mumodoreen25@gmail.com>
2023-02-15 11:14:20 -0800

    not my genres

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join show titles and genre
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.show_id ASC;
