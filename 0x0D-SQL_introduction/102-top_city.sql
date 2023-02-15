7f009e15976db0f4cd8bef4c8ada8c1790848933
Reen25 <mumodoreen25@gmail.com>
2023-02-15 00:53:08 -0800

    extra

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Displays the 3 cities with the highest temperatures between July and August
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 or `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
