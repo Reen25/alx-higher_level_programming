7449123cec86a1695b6a83965600793f7384c059
Reen25 <mumodoreen25@gmail.com>
2023-02-15 00:15:47 -0800

    best score no cheating

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,28 @@
+-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";-- Updates the score of Bob to 10 in the table in my MySQL server.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
