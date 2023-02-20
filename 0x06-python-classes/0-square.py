41d56edb06ebddf43e226e25e06a43c50a0ac043
Reen25 <mumodoreen25@gmail.com>
2023-02-19 23:35:12 -0800

    python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-
-    """ this module defines a empty class square """
-
-        
-
-            
-
-                class Square:
-
-                            """ empty class for task 0 """
-
-                                    pass
-
-
-
-
+""" this module defines a empty class square """
+class Square:
+    """ empty class for task 0 """
+pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
-
-    """ this module defines a class Square"""
-
-        
-
-            
-
-                class Square:
-
-                            """ class for Square"""
-
-                                    def __init__(self, size):
-
-                                                    """ Initialize class """
-
-                                                                self.__size = size
-
-
-
-
+""" this module defines a class Square"""
+class Square:
+    """ class for Square"""
+def __init__(self, size):
+    """ Initialize class """
+self.__size = size
