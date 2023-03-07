ad56babe9506f4be56f696f7a792c57441d26405
Reen25 <mumodoreen25@gmail.com>
2023-03-07 09:43:03 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-def raise_exception():
-    """Raise a TypeError exception."""
-    raise TypeError
+def no_c(my_string):
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
