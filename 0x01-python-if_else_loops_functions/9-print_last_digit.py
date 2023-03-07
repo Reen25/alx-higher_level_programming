d953255afdc7e86cda02375657a5cb8b62c636db
Reen25 <mumodoreen25@gmail.com>
2023-03-07 08:57:49 -0800

    print lat digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+# Author - Doreen Mumo
+
 def print_last_digit(number):
-    if number >= 0:
-        last_digit = number % 10
-        else:
-    last_digit = -(number % -10)
-    print(last_digit, end="")
-return last_digit
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
