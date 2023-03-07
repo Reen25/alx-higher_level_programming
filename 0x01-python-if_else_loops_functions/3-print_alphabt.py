872de73237807a09583c039c3115dfe270df897b
Reen25 <mumodoreen25@gmail.com>
2023-03-07 08:51:32 -0800

    3 alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 or i != 113:
-        continue
-else:
-    print("{:c}".format(i), end='')
+# Author - Bamidele Adefolaju
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
