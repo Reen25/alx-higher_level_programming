d8aec7731d85349735161dfc92b90995711c1e19
Reen25 <mumodoreen25@gmail.com>
2023-03-07 09:04:42 -0800

    else loop functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-        print("{:c}".format(i), end="")
+# Author -Doreen Mumo
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
