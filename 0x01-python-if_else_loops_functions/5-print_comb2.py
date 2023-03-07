0c78bdc56492621bb425607b158329afb7f0502a
Reen25 <mumodoreen25@gmail.com>
2023-03-07 08:52:54 -0800

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d},".format(i), end=" ")
-    print(i + 1)
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
