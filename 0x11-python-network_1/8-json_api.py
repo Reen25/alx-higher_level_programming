78368f295eacbf289f50cb1d5096f62714b80270
Reen25 <mumodoreen25@gmail.com>
2023-04-03 03:10:19 -0700

    jso api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
 """
-import sys
+Python script that sends a POST request to the URL and
+to an URL with the letter as a parameter
+"""
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    data = {'q': ""}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
+        data['q'] = sys.argv[1]
+    except:
+        pass
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        json_o = r.json()
+        if not json_o:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
+            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+    except:
         print("Not a valid JSON")
