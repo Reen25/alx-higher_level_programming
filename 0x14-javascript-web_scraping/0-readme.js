e96d3e6d8be0769e509d0e0faae8a73035a8ea99
Reen25 <mumodoreen25@gmail.com>
2023-04-28 07:00:51 -0700

    readme js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
-const file = process.arg[2];
 
-fs.readFile(process.arg[2], 'utf8', function (err, contents) {
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
 	if (contents === undefined) {
 		console.log(err);
 	} else {
