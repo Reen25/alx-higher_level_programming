72750eff7330e0735075b47cd2ab8dc5eef2e588
Reen25 <mumodoreen25@gmail.com>
2023-04-28 07:19:07 -0700

    starwars title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+request('https://swapi-api.alx-tools.com/api/films/' + id + '/', function (error, response, body) {
+	if (error == null) {
+		cons json = JSON.parse(body);
+		console.log(json.title);
+	}
+});
