f44dedf2c88952425a21c4da020ba61f3366d7d3
Reen25 <mumodoreen25@gmail.com>
2023-06-15 06:51:08 -0700

    stawars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
+
 const request = require('request');
-const id = process.argv[2];
-request('https://swapi-api.alx-tools.com/api/films/' + id + '/', function (error, response, body) {
-	if (error == null) {
-		cons json = JSON.parse(body);
-		console.log(json.title);
-	}
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
